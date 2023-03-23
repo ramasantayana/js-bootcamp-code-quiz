ce134e3e6bbf2253156f43588a4fde022e8e34f7
ramasantayana <rama.santayana@uconn.edu>
2023-03-22 23:22:59 -0400

    added code to script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+let questions = [
+    {
+        question: "Q1", 
+        ansChoices: ["A1", "A2", "A3", "A4"],
+        correctAnswer: "A3",
+    },
+    {
+        question: "Q2", 
+        ansChoices: ["A1", "A2", "A3", "A4"],
+        correctAnswer: "A1",
+    },
+    {
+        question: "Q3", 
+        ansChoices: ["A1", "A2", "A3", "A4"],
+        correctAnswer: "A4",
+    },
+    {
+        question: "Q4", 
+        ansChoices: ["A1", "A2", "A3", "A4"],
+        correctAnswer: "A2",
+    },
+    {
+        question: "Q5", 
+        ansChoices: ["A1", "A2", "A3", "A4"],
+        correctAnswer: "A1",
+    },
+    {
+        question: "Q6", 
+        ansChoices: ["A1", "A2", "A3", "A4"],
+        correctAnswer: "A4",
+    },
+];
